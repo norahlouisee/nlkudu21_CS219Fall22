5ae3e73329bc3b731461c5c1df93c58a80cb61a1
norah <norah.kuduk@gmail.com>
2022-08-25 15:47:47 -0400

    My first Java program.

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+// by convention, class names are capitalized
+// this is camelCase
+
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
+    }
+}
