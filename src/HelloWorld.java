59f452f410f5ba0efbc99a5765df453b62997571
norah <norah.kuduk@gmail.com>
2022-08-30 15:14:05 -0400

    update file to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,20 @@
 // this is camelCase
 
 public class HelloWorld {
+    /*
+    this is how to make a multiline block comment
+    another comment line
+     */
     public static void main(String [] args) {
         System.out.println("Hello World!");
 
         int x =5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022140857e23;  // scientific notation
+        double electronMass = 9.1093837015e-31;
 
     }
 }
