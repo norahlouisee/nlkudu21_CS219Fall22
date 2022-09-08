5f34a42597c7a2b770c2697d52a104c1cbe15d1f
norah <norah.kuduk@gmail.com>
2022-09-07 20:47:11 -0400

    cs219 homework 2 - day of the week updated

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,3 +1,9 @@
+/*
+ * Norah Kuduk
+ * 9/7/22
+ * HW2
+ */
+
 package HW2;
 
 import java.util.Scanner;
@@ -7,11 +13,12 @@ public class DayOfWeek {
     // function that takes in integer values for month, day, and year, then returns integer that
     // represents the day of the week that the date fell on
     public static int dayofweek(int m, int d, int y) {
-        // formula given to calculate the integer value of specific day
 
+        // formula given to calculate the integer value of specific day
         int y0 =  y - (14 - m)/12;
         int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
         int m0 = m + 12 * ((14 - m) / 12) - 2;
+
         // return final line of formula as int
         return (d + x + 31 * m0 / 12) % 7;
 
